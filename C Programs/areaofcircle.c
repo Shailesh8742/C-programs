bfa4cd68e19a7852d906e4769bf9449cfbbfcdd2
Shailesh8742 <118713991+Shailesh8742@users.noreply.github.com>
2022-11-21 12:41:57 +0530

    Update areaofcircle.c

--- C Programs/areaofcircle.c
+++ C Programs/areaofcircle.c
@@ -1,12 +1,24 @@
 #include<stdio.h>
+void add();
 void main()
 {
 	float area,radius,pi=3.14;
 	printf("\n Enter a value of Radius = ");
 	scanf("%f",&radius);
 	
-	
 	area=pi*radius*radius;
 	printf("\n Area of circle is = %.2f",area);
+	add();
+
+void add()
+{
+	int a,b,c;
+	printf("\n Enter A = ");
+	scanf("%d",&a);
+	printf("\n Enter B = ");
+	scanf("%d",&b);
+	c=a+b;
+	printf("\n Addition of Two Numbers is = %d",c);
+}
 	
 }
